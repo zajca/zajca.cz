6907a5edb067678c0e5a593c4a21bd9e61e5f64e
Martin Zajic <zajca@zajca.cz>
2019-01-09 08:36:56 +0100

    fix image component

--- components/Image.vue
+++ components/Image.vue
@@ -1,66 +1,67 @@
 <template lang="html">
-  <div v-lazy-container="{ selector: 'img' }" :class="`image-placeholder ${hasRadius}`">
-    <img :data-src="imageRequired" :data-loading="imageRequired.placeholder" :width="width" :height="height" :class="classes" :alt="alt" />
-  </div>
+	<div v-lazy-container="{ selector: 'img' }" :class="`image-placeholder ${hasRadius}`">
+		<img :data-src="imageRequired" :data-loading="imageRequired.placeholder" :width="width" :height="height" :class="classes" :alt="alt"/>
+	</div>
 </template>
 
 <script>
-export default {
-  props: {
-    imageURL: {
-      type: String,
-      required: true,
-    },
-    alt: {
-      type: String
-    },
-    width: {
-      type: String
-    },
-    height: {
-      type: String
-    },
-    classes: {
-      type: String
-    },
-    radius: {
-      type: Boolean,
-      default: false
-    }
-  },
-  computed: {
-    imageRequired () {
-      return require(`../assets/images/${this.imageURL}`)
-    },
-    hasRadius () {
-      return this.radius ? 'image-placeholder--radius' : ''
-    }
-  }
-}
+	export default {
+		props: {
+			imageURL: {
+				type: String,
+				required: true,
+			},
+			alt: {
+				type: String
+			},
+			width: {
+				type: String
+			},
+			height: {
+				type: String
+			},
+			classes: {
+				type: String
+			},
+			radius: {
+				type: Boolean,
+				default: false
+			}
+		},
+		computed: {
+			imageRequired() {
+				return require(`../assets/images/${this.imageURL}`)
+			},
+			hasRadius() {
+				return this.radius ? 'image-placeholder--radius' : ''
+			}
+		}
+	}
 </script>
 
-<style scoped lang="scss">
+<style>
 
-.image-placeholder {
-  overflow: hidden;
-  line-height: 0;
+	.image-placeholder {
+		overflow: hidden;
+		line-height: 0;
+	}
 
-  &--radius {
-    border-radius: 100%;
-  }
-}
+	.image-placeholder--radius {
+		border-radius: 100%;
+	}
 
-img {
-  transition: all ease .3s;
-  opacity: 0;
+	img {
+		transition: all ease .3s;
+		opacity: 0;
+	}
 
-  &[lazy='loading'] {
-    opacity: 1;
-    filter: blur(15px);
-  }
-  &[lazy='loaded'] {
-    opacity: 1;
-  }
-}
+	img[lazy='loading'] {
+		opacity: 1;
+		filter: blur(15px);
+	}
+
+	img[lazy='loaded'] {
+		opacity: 1;
+	}
 
 </style>
